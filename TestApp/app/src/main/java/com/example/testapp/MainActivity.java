93ea895ff143fa374cf861f6bb901e8198b03673
Dasha Sermiazhko <dachuleshka@mail.ru>
2020-05-23 12:37:40 +0300

    Implementing of the functionality Yes and No buttons

--- TestApp/app/src/main/java/com/example/testapp/MainActivity.java
+++ TestApp/app/src/main/java/com/example/testapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.testapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnYes = findViewById(R.id.btnYes);
+        btnYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.correct_answer,Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        Button btnNow = findViewById(R.id.btbNo);
+        btnNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.incorrect_answer,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
